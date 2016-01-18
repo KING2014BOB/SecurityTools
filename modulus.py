1762ed0e9866cbe18448039ac2faf02de09df3c2
SeanJames <a.sean_james@hotmail.com>
2016-01-18 14:29:24 +0000

    Simple Change

--- modulus.py
+++ modulus.py
@@ -1,4 +1,4 @@
-def seanMod(largenumber,n):
+def myMod(largenumber,n):
 	#Divide the number by the n in mod(n)
 	x = (float(largenumber) / float(n))
 
@@ -9,20 +9,20 @@ def seanMod(largenumber,n):
 	answer = int(round(answer))
 	return(answer)
 
-def pythonMod(largenumber,n):
+def defaultpythonMod(largenumber,n):
 	return( largenumber % n)
 
 
 def checker(largenumber,n):
 
 	test1 = pythonMod(largenumber,n)
-	test2 = seanMod(largenumber,n)
+	test2 = myMod(largenumber,n)
 
 	if (test1 == test2):
-		print("Good code(Results: %d, %d)"  % (test1,test2)  )
+		print("Works fine(Results: %d, %d)"  % (test1,test2)  )
 
 	else:
-		print("try harder Sean")
+		print("Something has gone wrong")
 
 checker(590,3)
 #checker()
